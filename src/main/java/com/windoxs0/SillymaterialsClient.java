a5c8ba8203e82d90d24d303e7f4e99cbd513fa3a
windoXs0 <lvn743221b@gmail.com>
2025-07-28 10:40:48 +0000

    initial commit actually

---
+++ src/main/java/com/windoxs0/SillymaterialsClient.java
@@ -0,0 +1,11 @@
+package com.windoxs0;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SillymaterialsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/windoxs0/SillymaterialsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.windoxs0;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SillymaterialsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
